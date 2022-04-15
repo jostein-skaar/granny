c650524e0b3c07cbcbd534ace70bdbb7d2dac15b
Jostein Skaar <jostein.skaar@gmail.com>
2022-04-15 22:32:34 +0200

    Remove auto mainfest for VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     VitePWA({
+      manifest: false,
       registerType: 'autoUpdate',
       workbox: {
         globPatterns: ['**/*.{js,css,html,png,json,svg,webmanifest}'],
