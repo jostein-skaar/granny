cfb80b40642e7a86cbb092aaa400bfc1d2624856
Jostein Skaar <jostein.skaar@gmail.com>
2022-04-15 21:48:45 +0200

    Pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default {
+export default defineConfig({
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
@@ -9,4 +10,15 @@ export default {
       },
     }),
   ],
-};
+});
+
+// export default {
+//   plugins: [
+//     VitePWA({
+//       registerType: 'autoUpdate',
+//       workbox: {
+//         globPatterns: ['**/*.{js,css,html,png,json,svg,webmanifest}'],
+//       },
+//     }),
+//   ],
+// };
